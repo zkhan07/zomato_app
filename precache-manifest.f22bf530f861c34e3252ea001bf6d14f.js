ed381ca2e8fdfe873f40f3e06ede437274fa3fd4
zaid <khanzaid786786.kz@gmail.com>
2020-02-27 09:34:17 +0530

    Updates

---
+++ precache-manifest.f22bf530f861c34e3252ea001bf6d14f.js
@@ -0,0 +1,478 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1a2e17febe508a891204903f157ba4",
+    "url": "/zomato_app/index.html"
+  },
+  {
+    "revision": "1eeeb6caa68a5e95aa18",
+    "url": "/zomato_app/static/css/9.9b741ea6.chunk.css"
+  },
+  {
+    "revision": "9cdf000c328451deeedf",
+    "url": "/zomato_app/static/css/main.4095f4cb.chunk.css"
+  },
+  {
+    "revision": "c67c5f271d461739a758",
+    "url": "/zomato_app/static/js/0.15889689.chunk.js"
+  },
+  {
+    "revision": "86dc553be47e7a436000",
+    "url": "/zomato_app/static/js/1.678332e9.chunk.js"
+  },
+  {
+    "revision": "edac63329835cd7f3fe4",
+    "url": "/zomato_app/static/js/10.5daad26c.chunk.js"
+  },
+  {
+    "revision": "af04b9c2f4cd918dc822",
+    "url": "/zomato_app/static/js/11.42df2eeb.chunk.js"
+  },
+  {
+    "revision": "e6e10a7afa531ba91cb2",
+    "url": "/zomato_app/static/js/12.fde46613.chunk.js"
+  },
+  {
+    "revision": "c66fedb5f6257eff21fb",
+    "url": "/zomato_app/static/js/13.57cdf6b6.chunk.js"
+  },
+  {
+    "revision": "585137fdff3160cec508",
+    "url": "/zomato_app/static/js/14.3c672abf.chunk.js"
+  },
+  {
+    "revision": "8f525598dd6e5afe1525",
+    "url": "/zomato_app/static/js/15.c8b151bc.chunk.js"
+  },
+  {
+    "revision": "d51fdcc044786220dd12",
+    "url": "/zomato_app/static/js/16.8beda748.chunk.js"
+  },
+  {
+    "revision": "15a30aa46ccf2b5df09e",
+    "url": "/zomato_app/static/js/17.5337bda7.chunk.js"
+  },
+  {
+    "revision": "2d79bf5ea8f8face2b9b",
+    "url": "/zomato_app/static/js/18.807a6ff5.chunk.js"
+  },
+  {
+    "revision": "ff71846a3fe8009d993f",
+    "url": "/zomato_app/static/js/19.5c1679bd.chunk.js"
+  },
+  {
+    "revision": "8039040282477c4797dc",
+    "url": "/zomato_app/static/js/2.f7bfc056.chunk.js"
+  },
+  {
+    "revision": "20c22dbf2cf625b6a1cc",
+    "url": "/zomato_app/static/js/20.109ab6eb.chunk.js"
+  },
+  {
+    "revision": "00177f5e60d46380ccbe",
+    "url": "/zomato_app/static/js/21.7ec101ba.chunk.js"
+  },
+  {
+    "revision": "60b9e37f2cf4397873b4",
+    "url": "/zomato_app/static/js/22.a4016f22.chunk.js"
+  },
+  {
+    "revision": "1838596692c579328714",
+    "url": "/zomato_app/static/js/23.1632b43e.chunk.js"
+  },
+  {
+    "revision": "ae8e12f54c585d17fea8",
+    "url": "/zomato_app/static/js/24.d12ed884.chunk.js"
+  },
+  {
+    "revision": "381fd40bf0319520f3d8",
+    "url": "/zomato_app/static/js/25.c4097790.chunk.js"
+  },
+  {
+    "revision": "9ab9440780b889d92438",
+    "url": "/zomato_app/static/js/26.c5afebf0.chunk.js"
+  },
+  {
+    "revision": "d17c5194009299426c77",
+    "url": "/zomato_app/static/js/27.b48653c6.chunk.js"
+  },
+  {
+    "revision": "d92107e5fc8626ef700c",
+    "url": "/zomato_app/static/js/28.df1da0b6.chunk.js"
+  },
+  {
+    "revision": "f7bdae0fac1082a7e662",
+    "url": "/zomato_app/static/js/29.62771df2.chunk.js"
+  },
+  {
+    "revision": "5ec8b4f4a178c977e417",
+    "url": "/zomato_app/static/js/3.08d1fa9e.chunk.js"
+  },
+  {
+    "revision": "023057336fa0498f1ca1",
+    "url": "/zomato_app/static/js/30.2f8c353f.chunk.js"
+  },
+  {
+    "revision": "b67cca3d13fa0f70912e",
+    "url": "/zomato_app/static/js/31.aeaae325.chunk.js"
+  },
+  {
+    "revision": "3d2d89e26a2b5eeac5d1",
+    "url": "/zomato_app/static/js/32.cc9b3594.chunk.js"
+  },
+  {
+    "revision": "52e71bc384c6c25c870f",
+    "url": "/zomato_app/static/js/33.0b686c80.chunk.js"
+  },
+  {
+    "revision": "7ac46a4d4191a27a25c0",
+    "url": "/zomato_app/static/js/34.4f4cd5e2.chunk.js"
+  },
+  {
+    "revision": "8fa27cbfc2e6c1b5b541",
+    "url": "/zomato_app/static/js/35.ff8370c0.chunk.js"
+  },
+  {
+    "revision": "981523b24986dc764c01",
+    "url": "/zomato_app/static/js/36.3f25d93e.chunk.js"
+  },
+  {
+    "revision": "48f0ca4fddc8cac11fba",
+    "url": "/zomato_app/static/js/37.53eee0e4.chunk.js"
+  },
+  {
+    "revision": "456a80fefd502b15b34b",
+    "url": "/zomato_app/static/js/38.4fed2130.chunk.js"
+  },
+  {
+    "revision": "04468db0124092d04d3b",
+    "url": "/zomato_app/static/js/39.671ca50e.chunk.js"
+  },
+  {
+    "revision": "c7b03a216edc89601fa4",
+    "url": "/zomato_app/static/js/4.854e862b.chunk.js"
+  },
+  {
+    "revision": "165e011fa48ce58bba2e",
+    "url": "/zomato_app/static/js/40.1a5e026b.chunk.js"
+  },
+  {
+    "revision": "f6500a53db232d67935a",
+    "url": "/zomato_app/static/js/41.9b245ebd.chunk.js"
+  },
+  {
+    "revision": "00b6997e8c6cb955ffe0",
+    "url": "/zomato_app/static/js/42.5c0512b1.chunk.js"
+  },
+  {
+    "revision": "b5b409b5f579d03b5bb0",
+    "url": "/zomato_app/static/js/43.008d84d9.chunk.js"
+  },
+  {
+    "revision": "add9542d94ae458544ec",
+    "url": "/zomato_app/static/js/44.3082424f.chunk.js"
+  },
+  {
+    "revision": "59fa51b96d69b4343bda",
+    "url": "/zomato_app/static/js/45.710a9775.chunk.js"
+  },
+  {
+    "revision": "98a884d07212c9257720",
+    "url": "/zomato_app/static/js/46.26919417.chunk.js"
+  },
+  {
+    "revision": "a6fb8e54efc2edb50c14",
+    "url": "/zomato_app/static/js/47.d6616945.chunk.js"
+  },
+  {
+    "revision": "84c4db2af5142fe2741c",
+    "url": "/zomato_app/static/js/48.6eb35b0c.chunk.js"
+  },
+  {
+    "revision": "5a03fca8d60500baf6c1",
+    "url": "/zomato_app/static/js/49.4dafbad3.chunk.js"
+  },
+  {
+    "revision": "69c06a8f5132d4daf863",
+    "url": "/zomato_app/static/js/5.67053918.chunk.js"
+  },
+  {
+    "revision": "7098dbf8fe5eed7c2e52",
+    "url": "/zomato_app/static/js/50.7bca190d.chunk.js"
+  },
+  {
+    "revision": "727791e07b1a8d5069de",
+    "url": "/zomato_app/static/js/51.acae4083.chunk.js"
+  },
+  {
+    "revision": "598ce7618c0b18c7b748",
+    "url": "/zomato_app/static/js/52.f010bdc8.chunk.js"
+  },
+  {
+    "revision": "a41f657451bf649bafaa",
+    "url": "/zomato_app/static/js/53.401276b4.chunk.js"
+  },
+  {
+    "revision": "377b6cbd9e873c8f36ea",
+    "url": "/zomato_app/static/js/54.d51510de.chunk.js"
+  },
+  {
+    "revision": "b8134b4b0fea69c86bd8",
+    "url": "/zomato_app/static/js/55.ca252bc4.chunk.js"
+  },
+  {
+    "revision": "eeba84e4fbaa9577d5df",
+    "url": "/zomato_app/static/js/56.ada86347.chunk.js"
+  },
+  {
+    "revision": "3a75ddb3746669a2946e",
+    "url": "/zomato_app/static/js/57.dd52255b.chunk.js"
+  },
+  {
+    "revision": "3930888b0a09341d1dda",
+    "url": "/zomato_app/static/js/58.65713ddc.chunk.js"
+  },
+  {
+    "revision": "0f5aacb2b5342d94d5b6",
+    "url": "/zomato_app/static/js/59.1d98cfd5.chunk.js"
+  },
+  {
+    "revision": "407b04f50dde3391d669",
+    "url": "/zomato_app/static/js/6.6db2cccc.chunk.js"
+  },
+  {
+    "revision": "296e9ef3f11454801a48",
+    "url": "/zomato_app/static/js/60.dae53b71.chunk.js"
+  },
+  {
+    "revision": "04639f8a22f27e434404",
+    "url": "/zomato_app/static/js/61.3a6265e6.chunk.js"
+  },
+  {
+    "revision": "2e5feb247752a8a8d4cd",
+    "url": "/zomato_app/static/js/62.8e4219f1.chunk.js"
+  },
+  {
+    "revision": "2fc19047e92745ecac0a",
+    "url": "/zomato_app/static/js/63.2adfa54f.chunk.js"
+  },
+  {
+    "revision": "bd62a96445562d12fda9",
+    "url": "/zomato_app/static/js/64.f0d56655.chunk.js"
+  },
+  {
+    "revision": "fa7c59c076262270a8da",
+    "url": "/zomato_app/static/js/65.6c42381e.chunk.js"
+  },
+  {
+    "revision": "d19573ef34027a8bb8b5",
+    "url": "/zomato_app/static/js/66.cc8bcbff.chunk.js"
+  },
+  {
+    "revision": "fbd3a49a938d65e13d26",
+    "url": "/zomato_app/static/js/67.50e6f692.chunk.js"
+  },
+  {
+    "revision": "e1c8eb07af82418ff843",
+    "url": "/zomato_app/static/js/68.e91af796.chunk.js"
+  },
+  {
+    "revision": "eaeff303b63b98a0618e",
+    "url": "/zomato_app/static/js/69.00c3b333.chunk.js"
+  },
+  {
+    "revision": "f9d4e7c27537310a25b4",
+    "url": "/zomato_app/static/js/70.745bc050.chunk.js"
+  },
+  {
+    "revision": "60d1f021a44b5a50653d",
+    "url": "/zomato_app/static/js/71.a29d5b41.chunk.js"
+  },
+  {
+    "revision": "290923e04d2a68d84b2e",
+    "url": "/zomato_app/static/js/72.82e58583.chunk.js"
+  },
+  {
+    "revision": "bb3f6e2ecbc6395fdcfd",
+    "url": "/zomato_app/static/js/73.96260308.chunk.js"
+  },
+  {
+    "revision": "1f5e2eb367f858749c31",
+    "url": "/zomato_app/static/js/74.39aadec8.chunk.js"
+  },
+  {
+    "revision": "38d4e5db4b405e5b16d8",
+    "url": "/zomato_app/static/js/75.b3a54230.chunk.js"
+  },
+  {
+    "revision": "87b66d8eca6812ea7ace",
+    "url": "/zomato_app/static/js/76.13e0b1c1.chunk.js"
+  },
+  {
+    "revision": "20ba4c887f311492b32b",
+    "url": "/zomato_app/static/js/77.5d90e719.chunk.js"
+  },
+  {
+    "revision": "2196ee2c99ef02f046ce",
+    "url": "/zomato_app/static/js/78.f7befed5.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/zomato_app/static/js/78.f7befed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2f94883e8a6d843ed9",
+    "url": "/zomato_app/static/js/79.f966b882.chunk.js"
+  },
+  {
+    "revision": "d20b8bc6d8392b8a0edf",
+    "url": "/zomato_app/static/js/80.2335e574.chunk.js"
+  },
+  {
+    "revision": "173eeb1ba8a039961a84",
+    "url": "/zomato_app/static/js/81.e72d5811.chunk.js"
+  },
+  {
+    "revision": "e17f0639ce10b0b8f8e2",
+    "url": "/zomato_app/static/js/82.07bca9dc.chunk.js"
+  },
+  {
+    "revision": "6f1443dc71a724c60ae0",
+    "url": "/zomato_app/static/js/83.7715b0cb.chunk.js"
+  },
+  {
+    "revision": "9835be716f0ec53d0570",
+    "url": "/zomato_app/static/js/84.5438da2e.chunk.js"
+  },
+  {
+    "revision": "ff3382bfa2f4a89190ca",
+    "url": "/zomato_app/static/js/85.5713e14f.chunk.js"
+  },
+  {
+    "revision": "134e3491a9c33bc3269b",
+    "url": "/zomato_app/static/js/86.6096d899.chunk.js"
+  },
+  {
+    "revision": "6cd80a0dd9d8f3694703",
+    "url": "/zomato_app/static/js/87.53afe0a9.chunk.js"
+  },
+  {
+    "revision": "75e9dc72167c6be59fe4",
+    "url": "/zomato_app/static/js/88.dc4fa5f1.chunk.js"
+  },
+  {
+    "revision": "a0b5b5fd19b748a93628",
+    "url": "/zomato_app/static/js/89.fc6be687.chunk.js"
+  },
+  {
+    "revision": "1eeeb6caa68a5e95aa18",
+    "url": "/zomato_app/static/js/9.22d5f066.chunk.js"
+  },
+  {
+    "revision": "68ba486219dc7972526df1416a899782",
+    "url": "/zomato_app/static/js/9.22d5f066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e6eb1ec7c63a718ecb",
+    "url": "/zomato_app/static/js/90.8eb41cbc.chunk.js"
+  },
+  {
+    "revision": "d9d9c3e419a598b72640",
+    "url": "/zomato_app/static/js/91.f436471a.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/zomato_app/static/js/91.f436471a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa97aa44304526df7cf",
+    "url": "/zomato_app/static/js/92.4e4cdd03.chunk.js"
+  },
+  {
+    "revision": "6e1a086a075134465c77",
+    "url": "/zomato_app/static/js/93.013f53a5.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/zomato_app/static/js/93.013f53a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b23e5817ec0341451c",
+    "url": "/zomato_app/static/js/94.16cdc45a.chunk.js"
+  },
+  {
+    "revision": "e5aa6cc5e6747fe70afc",
+    "url": "/zomato_app/static/js/95.2ec716a6.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/zomato_app/static/js/95.2ec716a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d0b4656228b959f32c",
+    "url": "/zomato_app/static/js/96.fbf14160.chunk.js"
+  },
+  {
+    "revision": "e6808afed7c8e392ffeb",
+    "url": "/zomato_app/static/js/97.c6f4a058.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/zomato_app/static/js/97.c6f4a058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdf000c328451deeedf",
+    "url": "/zomato_app/static/js/main.9033080f.chunk.js"
+  },
+  {
+    "revision": "23b905f95f0be5b2e906",
+    "url": "/zomato_app/static/js/runtime-main.c51baaa4.js"
+  },
+  {
+    "revision": "3a48e7fdad1b04125d95ce332da663db",
+    "url": "/zomato_app/static/media/f1.3a48e7fd.jpg"
+  },
+  {
+    "revision": "0d847f0d13df212924e14baa2188f92a",
+    "url": "/zomato_app/static/media/f2.0d847f0d.jpg"
+  },
+  {
+    "revision": "6c7990b57bea37f7297834c458ad06c0",
+    "url": "/zomato_app/static/media/f3.6c7990b5.jpg"
+  },
+  {
+    "revision": "bd4927be68eb4d29a76d8ac98c51b9ad",
+    "url": "/zomato_app/static/media/f4.bd4927be.jpg"
+  },
+  {
+    "revision": "5c32321ce840efb26b516c1acbf6b32d",
+    "url": "/zomato_app/static/media/f5.5c32321c.jpg"
+  },
+  {
+    "revision": "93143bba7b5ce84a5354dc13aff0fe16",
+    "url": "/zomato_app/static/media/f6.93143bba.jpg"
+  },
+  {
+    "revision": "03ea99183df6a3e60b120ce28c1ad46c",
+    "url": "/zomato_app/static/media/f7.03ea9918.jpg"
+  },
+  {
+    "revision": "22d5ae7ec66e89019a2289a46ce3a337",
+    "url": "/zomato_app/static/media/f8.22d5ae7e.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/zomato_app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/zomato_app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/zomato_app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/zomato_app/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ static/js/4.854e862b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpzomato_app=this.webpackJsonpzomato_app||[]).push([[4],{271:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(55),i=(o(15),function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.g)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var a=i.closest("ion-content");a&&a.componentOnReady().then((function(){Object(e.l)((function(){return a.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=4.854e862b.chunk.js.map
